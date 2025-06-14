9418d9192545c51af6b9ca7b47ba1dc9843a7b7e
chenyingxu <chenyingxu@zingfront.com>
2025-06-14 18:07:46 +0800

    敏感词检测接口

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
 from api.common.common_api import router as CommonRouter
+from api.text_api.text_api import router as TextRouter
 from midddlewares.TokenVerifyMiddleware  import TokenVerifyMiddleware
 
 app = FastAPI()
 
 app.include_router(CommonRouter, prefix="/api")
+app.include_router(TextRouter, prefix="/api")
 
 # app.add_middleware(TokenVerifyMiddleware)
 
