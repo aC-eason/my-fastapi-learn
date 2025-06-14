6a653d8c199bf44c83466cf84cc8538e5c09af5d
chenyingxu <chenyingxu@zingfront.com>
2025-06-14 18:19:21 +0800

    敏感词检测接口

--- app/api/text_api/text_api.py
+++ app/api/text_api/text_api.py
@@ -6,7 +6,7 @@ from service.text_service import text_service
 
 router = APIRouter(tags=["通用api"],prefix="/text", responses={404: {"description": "Not found"}})
 
-@router.post("sensitiveWord/check")
+@router.post("/sensitiveWord/check")
 def senesitive_word_check(request:Request,word_bdy:SensitiveWordCheck):
     has_sensitive_word, sensitive_word = text_service.check_word(word_bdy.content)
     resp = {
