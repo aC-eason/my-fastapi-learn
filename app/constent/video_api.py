783c1833f53b14b8a2874491d1575e1ead5cbde0
chenyingxu <chenyingxu@zingfront.com>
2025-06-15 09:31:58 +0800

    instagram download

--- app/constent/video_api.py
+++ app/constent/video_api.py
@@ -1,5 +1,5 @@
 
 class VideoAPI:
     #instargam api
-    INS_API= 'https://www.instagram.com/graphql/query?variables={"shortcode":"{}"}&doc_id=8845758582119845&server_timestamps=true'
+    INSTARGRAM_API= 'https://www.instagram.com/graphql/query?variables={"shortcode":"{}"}&doc_id=8845758582119845&server_timestamps=true'
     SNAPDOWNLOAD_API = 'https://snapdownloader.com/tools/instagram-downloader/download?url={}'
