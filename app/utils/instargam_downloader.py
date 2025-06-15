e2f13f4ecbce40ee18f148bc7f107f01e27a02a5
chenyingxu <chenyingxu@zingfront.com>
2025-06-15 10:28:35 +0800

    fix

--- app/utils/instargam_downloader.py
+++ app/utils/instargam_downloader.py
@@ -4,26 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 from utils.request_utils import send_request
 from utils.request_utils import random_ua
+
 # from constent.video_api import VideoAPI
 
 
 def instargam_api(ins_id: str):
     ins_url = []
     type = 0
+    headers = {
+        "Cookie": "csrftoken=bPxhysJmcDG2PrWDQrZt4LinqVWsN3dG; ig_did=4C4F0A91-FB84-42F1-B8F2-5F4FC37C73B4; ig_nrcb=1; mid=aE4tKgAEAAG87EhF9RCXNdogAurx"
+    }
     request_url = (
         'https://www.instagram.com/graphql/query?variables={"shortcode":"'
         + ins_id
         + '"}&doc_id=8845758582119845&server_timestamps=true'
     )
 
-    response = send_request(url=request_url)
+    response = send_request(url=request_url,headers=headers, method="GET")
     if not response or response.status_code != 200:
         return None
     result = json.loads(response.text)
     status = result.get("status")
     if status and status == "ok":
         is_video = result.get("is_video")
-        result = result.get("data",{}).get("xdt_shortcode_media",{})
+        result = result.get("data", {}).get("xdt_shortcode_media", {})
         if is_video:
             type = 1
             if result.get("video_url") and result.get("video_url") != "":
@@ -38,15 +42,15 @@ def instargam_api(ins_id: str):
                     ]
             elif result.get("display_url") and result.get("display_url") != "":
                 ins_url.append(result.get("display_url"))
-                 
-                
 
     return ins_url, type
 
 
 def parse_snapdownload(url: str):
     ins_url = []
-    request_url =  f"https://snapdownloader.com/tools/instagram-downloader/download?url={url}"   
+    request_url = (
+        f"https://snapdownloader.com/tools/instagram-downloader/download?url={url}"
+    )
     response = requests.get(request_url)
     html = response.text
 
