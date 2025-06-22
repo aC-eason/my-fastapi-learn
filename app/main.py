4b15a1ed416a25be6c51af650c0500f5747992e1
chenyingxu <chenyingxu@zingfront.com>
2025-06-22 23:08:59 +0800

    add router

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from starlette.middleware.cors import CORSMiddleware
 from api.text_api.text_api import router as TextRouter
 from api.common.common_api import router as CommonRouter
 from api.video_api.video_api import router as VideoRouter
+from api.user.user_api import router as UserRouter
 from midddlewares.TokenVerifyMiddleware import TokenVerifyMiddleware
 
 app = FastAPI()
@@ -11,6 +12,7 @@ app = FastAPI()
 app.include_router(CommonRouter, prefix="/api")
 app.include_router(TextRouter, prefix="/api")
 app.include_router(VideoRouter, prefix="/api")
+app.include_router(UserRouter, prefix="/api")
 
 # app.add_middleware(TokenVerifyMiddleware)
 
